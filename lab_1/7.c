eb3a68813cb04fa36639e7db10bcc9fff6824ea3
Nikita <zhnikita3@gmail.com>
2024-02-15 18:23:18 +0300

    Added error rate output

--- lab_1/7.c
+++ lab_1/7.c
@@ -6,12 +6,14 @@
 #define ERR_OK 0
 #define ERR_IO 1
 
-#define ABS(X) (X) < 0 ? -(X) : (X)
+#define ABS(X) ((X) < 0 ? -(X) : (X))
 
 int main(void)
 {
     float x, e;
     float s, f, cur;
+    float errRateAbs, errRateRel;
+
     int i = 1;
 
     printf("Input numbers: ");
@@ -34,6 +36,11 @@ int main(void)
         // printf("cur %f; prev %f; cur-prev %f\n", cur, prev, ABS(cur - prev)); // dbg
     }
 
-    printf("f(x): %f s(x): %f\n", f, s);
+    errRateAbs = ABS(f - s);
+    errRateRel = ABS(f - s) / ABS(f);
+    
+    printf("f(x): %f\ns(x): %f\n", f, s);
+    printf("Absolute error rate: %f\nRelative error rate: %f\n", errRateAbs, errRateRel);
+
     return ERR_OK;
 }
